aa54e723897bd0f8d9cc97cc82a92103ca8729d1
3ritesh <rk7480095@gmail.com>
2026-03-03 00:23:45 +0530

    first1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 31337,
+    },
+    polygon_mumbai: {
+      url: process.env.NEXT_PUBLIC_POLYGON_MUMBAI_RPC || "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY],
+    },
+    sepolia: {
+      url: process.env.NEXT_PUBLIC_SEPOLIA_RPC || "https://rpc.sepolia.org",
+      accounts: [PRIVATE_KEY],
     },
   },
 };
